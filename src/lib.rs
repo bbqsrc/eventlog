5aa01cc0564e9e7d25a58a15f227601998135136
Jeroen C. van Gelderen <jeroen@vangelderen.org>
2019-09-06 03:33:31 +0000

    Use dyn keyword.

--- src/lib.rs
+++ src/lib.rs
@@ -42,7 +42,7 @@ impl fmt::Display for Error {
 }
 
 impl error::Error for Error {
-    fn cause(&self) -> Option<&error::Error> {
+    fn cause(&self) -> Option<&dyn error::Error> {
         match *self {
             Error::Io(ref err) => Some(err),
             Error::ExePathNotFound => None,
